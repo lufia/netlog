64f56ef40eeb52c5a94c3a9d6d58a5a69c3e03df
kadota <kadota@fenrir.co.jp>
2015-12-04 13:41:43 +0900

    Add DefaultLogger to write stdout

--- netlog.go
+++ netlog.go
@@ -1,5 +1,10 @@
 package netlog
 
+import (
+	"log"
+	"os"
+)
+
 type Logger interface {
 	Info(format string, v ...interface{})
 	Warning(format string, v ...interface{})
@@ -15,3 +20,42 @@ const (
 	LOG_SERVICE     = facilityService
 	LOG_SECURITY    = facilitySecurity
 )
+
+var (
+	DefaultLogger Logger = consoleLogger{}
+)
+
+type consoleLogger struct{}
+
+func (c consoleLogger) Info(format string, v ...interface{}) {
+	log.Printf(format, v...)
+}
+
+func (c consoleLogger) Warning(format string, v ...interface{}) {
+	log.Printf(format, v...)
+}
+
+func (c consoleLogger) Err(format string, v ...interface{}) {
+	log.Printf(format, v...)
+}
+
+func (c consoleLogger) Crit(format string, v ...interface{}) {
+	log.Printf(format, v...)
+	os.Exit(2)
+}
+
+func Info(format string, v ...interface{}) {
+	DefaultLogger.Info(format, v...)
+}
+
+func Warning(format string, v ...interface{}) {
+	DefaultLogger.Warning(format, v...)
+}
+
+func Err(format string, v ...interface{}) {
+	DefaultLogger.Err(format, v...)
+}
+
+func Crit(format string, v ...interface{}) {
+	DefaultLogger.Crit(format, v...)
+}
